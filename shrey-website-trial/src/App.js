4aa0d4eb53d888d0cb5ae1214887945b6be33663
Shrey Dubey <wshrey09@gmail.com>
2020-06-25 00:17:38 +0530

    Made

---
+++ shrey-website-trial/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import Particles from 'react-particles-js';
+import './App.css';
+import smallDeer from './small-deer.svg'
+import ParticleComponent from './components/particleContainer/particleContainer.component'
+
+function App() {
+  return (
+          <div>
+            <ParticleComponent />
+          </div>
+      );
+  };
+
+export default App;
