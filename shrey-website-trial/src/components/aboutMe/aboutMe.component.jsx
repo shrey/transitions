1aaecc963a20aa6aa1b4347a2991ce85065267d5
Shrey Dubey <wshrey09@gmail.com>
2020-06-25 15:28:30 +0530

    Yayy

--- shrey-website-trial/src/components/aboutMe/aboutMe.component.jsx
+++ shrey-website-trial/src/components/aboutMe/aboutMe.component.jsx
@@ -3,11 +3,14 @@ import ScrollAnimation from 'react-animate-on-scroll';
 import Fade from 'react-reveal/Fade';
 const AboutMe = () => {
     return(
-        <div>
-        <Fade left delay={500} opposite={true}>
-          <h1>React Reveal</h1>
+
+        <Fade delay={100} unmountOnExit={true} >
+          <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100%" ,width: "100%"}} >
+
+          <h1>Shrey Dubey</h1>
+          </div>
         </Fade>
-      </div>
+
     )
 }
 
