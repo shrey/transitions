4fa1c40e6d36dd04122dda6f53b1e8ab1781622e
Shrey Dubey <wshrey09@gmail.com>
2020-06-28 02:56:27 +0530

    more

--- shrey-website-trial/src/pages/about/about.component.jsx
+++ shrey-website-trial/src/pages/about/about.component.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import Image from 'react-bootstrap/Image'
 import './about.styles.scss'
 import ShreyImage from '../../github_photo.jpg'
+import Fade from 'react-reveal/Fade';
 
 const About = () => {
     return (
         <div className="about-container">
+            <Fade delay={100}>
             <div className="about-title">
                 About Me
             </div>
+            </Fade>
             <div className="about-content">
+            <Fade delay={200}>
             <div className="about-description">
                 Hey, I'm Shrey, a Computer Engineering student at Delhi Technological University, I like to build things for the web.
                 I build high quality web applications having clear and precise design with quick and modern backends.
             <div style={{marginTop: "20px"}}>
-                I've recently been working on :-
+                I've recently been working with :
             </div>
             <div className="about-skills">
                 <div className="about-skills-left">
@@ -34,9 +38,13 @@ const About = () => {
                 </div>
             </div>
             </div>
+            </Fade>
+            <Fade delay={300}>
             <div className="about-image">
-            <Image src={ShreyImage} rounded className="about-image-container"/>
+            <Image src={ShreyImage} className="about-image-container"/>
             </div>
+
+            </Fade>
 
             </div>
         </div>
