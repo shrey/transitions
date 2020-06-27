867b009ea994e496aa649ce90ed338a8f5e74331
Shrey Dubey <wshrey09@gmail.com>
2020-06-28 00:10:37 +0530

    Bleh

--- shrey-website-trial/src/pages/about/about.component.jsx
+++ shrey-website-trial/src/pages/about/about.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-
+import Image from 'react-bootstrap/Image'
 import './about.styles.scss'
+import ShreyImage from '../../github_photo.jpg'
 
 const About = () => {
     return (
@@ -12,10 +13,30 @@ const About = () => {
             <div className="about-description">
                 Hey, I'm Shrey, a Computer Engineering student at Delhi Technological University, I like to build things for the web.
                 I build high quality web applications having clear and precise design with quick and modern backends.
-                <br />
+            <div style={{marginTop: "20px"}}>
                 I've recently been working on :-
             </div>
-            <div className="about-image">Image comes here</div>
+            <div className="about-skills">
+                <div className="about-skills-left">
+                    <div className="about-skill">Javascript (ES6+)</div>
+                    <div className="about-skill">ReactJS</div>
+                    <div className="about-skill">NodeJS</div>
+                    <div className="about-skill">C++</div>
+                    <div className="about-skill">Python</div>
+                    <div className="about-skill">NodeJS</div>
+                </div>
+                <div className="about-skills-right">
+                    <div className="about-skill">Typescript</div>
+                    <div className="about-skill">Express</div>
+                    <div className="about-skill">HTML and (S)CSS</div>
+                    <div className="about-skill">Git and Github</div>
+                    <div className="about-skill">MongoDB</div>
+                </div>
+            </div>
+            </div>
+            <div className="about-image">
+            <Image src={ShreyImage} rounded className="about-image-container"/>
+            </div>
 
             </div>
         </div>
